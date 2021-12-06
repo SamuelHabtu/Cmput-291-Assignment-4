ec548614d53a6ae15fe0c73dcdd61492a5e2be5b
cinnamonhater <89957213+cinnamonhater@users.noreply.github.com>
2021-12-06 06:39:11 -0700

    Updated with Query-needs to be changed

--- A4Q4Norm.py
+++ A4Q4Norm.py
@@ -1,11 +1,29 @@
-def main():
-    import pymongo
+import pymongo
+
+def main()
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["A4dbNorm"]
-    mycol = mydb[tracks]
-    myquery = {//query in here}
+    print(mydb.list_collection_names())
+    mycol = mydb["tracks"]
+    myquery = {'''db.artists.aggregate([
+    { $lookup:
+        {
+           from: "tracks",
+           localField: "artist_id",
+           foreignField: "artist_ids",
+           as: "Tracks"
+        }
+    },
+    { $match:
+    {
+       release_date:{$gt: new ISODate("1950-01-01T:23:59:59"}
+    },
+     { $group: { _id:"$_id", name: "$name", t_name:"$Tracks.name"}
+
+]); 
+                 '''}
     mydoc = mycol.find(myquery)
-    pass
+    
 
 if __name__ == "__main__":
     main()
