e20893e27dcb44e7e7124fb63df57cbd3e0ac630
cinnamonhater <89957213+cinnamonhater@users.noreply.github.com>
2021-12-06 16:29:55 -0700

    :/

--- A4Q4Norm.py
+++ A4Q4Norm.py
@@ -7,7 +7,23 @@ def main():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["A4dbEmbed"]
     col = db["artiststracks"]
-    query = col.aggregate()
+    query = db.artists.aggregate([
+            { $lookup:
+            {
+            from: "tracks",
+            localField: "artist_id",
+            foreignField: "artist_ids",
+            as: "Tracks"
+            }
+            },
+            { $match:
+            {
+            release_date:{$gt: new ISODate("1950-01-01T:23:59:59"}
+            },
+            { $group: { _id:"$_id", name: "$name", t_name:"$Tracks.name"}
+
+            ])
+
     for doc in query:
             print(doc)
 
