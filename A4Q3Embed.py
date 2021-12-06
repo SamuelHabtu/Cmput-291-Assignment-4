4efe5b409b5e6e8367121c51676edea4047612d1
cinnamonhater <89957213+cinnamonhater@users.noreply.github.com>
2021-12-06 06:15:42 -0700

    Updated w what I think the query is pt. 2

--- A4Q3Embed.py
+++ A4Q3Embed.py
@@ -1,11 +1,19 @@
-def main():
-    import pymongo
+import pymongo
+
+def main()
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["A4dbEmbed"]
-    mycol = mydb[tracks]
-    myquery = {//query in here}
+    mydb = myclient["A4dbNorm"]
+    
+    myquery = {'''db.mydb.aggregate(
+    [
+        { $group: {_id:"$artist_ids", 
+        total_length: {$sum: "$duration"}, 
+        artist_ids:{$first :"$artist_ids"} } }
+    ]
+    );
+    '''}
     mydoc = mycol.find(myquery)
-    pass
+    
 
 if __name__ == "__main__":
     main()
