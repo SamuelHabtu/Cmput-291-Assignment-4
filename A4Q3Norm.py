6001f58f151f4d146db099617cd9cc4854cf6c99
cinnamonhater <89957213+cinnamonhater@users.noreply.github.com>
2021-12-06 06:01:05 -0700

    Adding query 3

--- A4Q3Norm.py
+++ A4Q3Norm.py
@@ -1,11 +1,20 @@
-def main():
-    import pymongo
+import pymongo
+
+def main()
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["A4dbNorm"]
-    mycol = mydb[tracks]
-    myquery = {//query in here}
+    print(mydb.list_collection_names())
+    mycol = mydb["tracks"]
+    myquery = {'''db.tracks.aggregate(
+    [
+        { $group: {_id:"$artist_ids", 
+        total_length: {$sum: "$duration"}, 
+        artist_ids:{$first :"$artist_ids"} } }
+    ]
+    );
+    '''}
     mydoc = mycol.find(myquery)
-    pass
+    
 
 if __name__ == "__main__":
     main()
